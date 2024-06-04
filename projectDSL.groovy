f5e8cf4fbc0d16ff7760a053ce36d96374abf549
Veera <164357439+vra0613@users.noreply.github.com>
2024-06-04 13:31:13 +0530

    Update projectDSL.groovy

--- projectDSL.groovy
+++ projectDSL.groovy
@@ -1,6 +1,6 @@
 job('Project Build-DSL') {
   description("Maven Build Project using Job DSL")
   scm {
-    git("https://github.com/vra0613/hello-world.git", master)
+    checkout scmGit(branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'GITHUB', url: 'https://github.com/vra0613/hello-world.git']])
   }
 }
