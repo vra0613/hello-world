9a6458ab4ce2f8895b427afbbbfc2488d0c87035
Veera <164357439+vra0613@users.noreply.github.com>
2024-06-04 13:11:18 +0530

    Create projectDSL.groovy

---
+++ projectDSL.groovy
@@ -0,0 +1,15 @@
+job('Project Build-DSL') {
+  description("Maven Build Project using Job DSL")
+  scm {
+    git("git@github.com:vra0613/hello-world.git", master)
+  }
+  trigger {
+    scm('*****')
+  }
+  steps {
+    maven('clean package install', 'hello-world/pom.xml')
+  }
+  publishers {
+    archiveArtifacts '**/*.jar'
+  }
+}
